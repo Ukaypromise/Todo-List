53a48ef5bb730d87996d3ba48d7c1316fd790985
Engr. Promise <85847249+Ukaypromise@users.noreply.github.com>
2022-08-10 03:04:37 +0100

    Create modules and dynamically display todo items

--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
 import _ from "lodash";
 import "./style.css";
+import showTodo from "../modules/showTodo.js";
 
-function component() {
-  const element = document.createElement("div");
 
-    // Lodash, now imported by this script
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
-  element.classList.add("hello");
-
-  return element;
-}
-
-document.body.appendChild(component());
+window.addEventListener("DOMContentLoaded", () => {
+  showTodo();
+ 
+});
