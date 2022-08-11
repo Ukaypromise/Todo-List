45e24370488882d1e87d94d439e5a620fb61afa5
Engr. Promise <85847249+Ukaypromise@users.noreply.github.com>
2022-08-11 10:26:50 +0100

    Refactor showTodo to dynamically display content

--- src/index.js
+++ src/index.js
@@ -1,28 +1,49 @@
 import "./style.css";
 
 const taskInput = document.querySelector(".input-container input");
- let todos = JSON.parse(localStorage.getItem("todo-list")); // get todos from localStorage
+let todos = JSON.parse(localStorage.getItem("todo-list"));  // get todos from localStorage
 
-// Show todos
-const todoList = document.querySelector(".todo-list");
+const list = document.querySelector("ul");
+const form = document.querySelector("form");
+todos = [];
+
 const showTodo = () => {
-  let li = '';
-  if(todos){
-    todos.forEach((todo, id) => {
-      li += `<li id="${id}"><input class="update" type="checkbox" id="${id}">${todo.text} <i id="${id}" class="fas fa-ellipsis-v dot main"></i><i class="fas fa-trash-alt delete main"></i> </li>        
-      `;
-    });
-    todoList.innerHTML = li;
-  }
-  
-}
-showTodo()
-
-  
+  list.innerHTML = "";
+  todos.forEach((todo) => {
+    const li = document.createElement("li");
+    const checked = document.createElement("input");
+    const text = document.createElement("input");
+    const menuIcon = document.createElement("span");
+    const deleteIcon = document.createElement("span");
+    li.setAttribute("class", "li-wrapper");
+    checked.type = "checkbox";
+    checked.name = "task-done";
+    checked.checked = false;
+    checked.id = todo.id;
+    text.type = "text";
+    text.name = "task";
+    text.value = todo.text;
+    text.id = todo.id;
+    text.disabled = true;
+    text.style.cursor = "pointer";
+    text.style.background = "none";
+    text.style.color = "black";
+    menuIcon.innerHTML = '<i class="fas fa-ellipsis-v " for="select"></i>';
+    deleteIcon.innerHTML = `< class="fas fa-trash-alt" id="${todo.id}"></i>`;
+    deleteIcon.style.display = "none";
+    li.appendChild(checked);
+    li.appendChild(text);
+    li.appendChild(menuIcon);
+    li.appendChild(deleteIcon);
+    list.appendChild(li);
+  });
+};
+showTodo();
 
 taskInput.addEventListener("keyup", (e) => {
+  e.preventDefault();
   let userTask = taskInput.value.trim();
-  if (e.key === "Enter" && userTask) {  
+  if (e.key === "Enter" && userTask) {
     if (!todos) {
       todos = []; //If no todo pass an empty array
     }
@@ -37,6 +58,4 @@ taskInput.addEventListener("keyup", (e) => {
     showTodo();
   }
 });
-
-
 
