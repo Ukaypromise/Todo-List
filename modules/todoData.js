0552461a5675767929ffe72803ebb416938eda81
Engr. Promise <85847249+Ukaypromise@users.noreply.github.com>
2022-08-11 01:57:41 +0100

    Add task to local storage

--- modules/todoData.js
+++ modules/todoData.js
@@ -1,19 +1,5 @@
-const todos = [
-  {
-    id: 1,
-    text: 'Take out the trash',
-    isCompleted: true,
-  },
-  {
-    id: 2,
-    text: 'Fix my old car',
-    isCompleted: false,
-  },
-  {
-    id: 3,
-    text: 'Visit my friend',
-    isCompleted: true,
-  },
+let todos = [
+ 
 ];
 
 export default todos;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,42 @@
-import './style.css';
-import showTodo from '../modules/showTodo.js';
+import "./style.css";
 
-window.addEventListener('DOMContentLoaded', () => {
-  showTodo();
+const taskInput = document.querySelector(".input-container input");
+ let todos = JSON.parse(localStorage.getItem("todo-list")); // get todos from localStorage
+
+// Show todos
+const todoList = document.querySelector(".todo-list");
+const showTodo = () => {
+  let li = '';
+  if(todos){
+    todos.forEach((todo, id) => {
+      li += `<li id="${id}"><input class="update" type="checkbox" id="${id}">${todo.text} <i id="${id}" class="fas fa-ellipsis-v dot main"></i><i class="fas fa-trash-alt delete main"></i> </li>        
+      `;
+    });
+    todoList.innerHTML = li;
+  }
+  
+}
+showTodo()
+
+  
+
+taskInput.addEventListener("keyup", (e) => {
+  let userTask = taskInput.value.trim();
+  if (e.key === "Enter" && userTask) {  
+    if (!todos) {
+      todos = []; //If no todo pass an empty array
+    }
+    taskInput.value = "";
+    let newTodo = {
+      id: todos.length + 1,
+      text: userTask,
+      isCompleted: false,
+    };
+    todos.push(newTodo); //Adding new task to the list
+    localStorage.setItem("todo-list", JSON.stringify(todos));
+    showTodo();
+  }
 });
+
+
+
